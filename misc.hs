8f197350839e64ca28048a9a16a52078255c8b8a
David Tchepak <dave@davesquared.net>
2011-12-08 01:14:56 +1100

    Adding combination generation and a few other misc practice functions.

--- misc.hs
+++ misc.hs
@@ -19,4 +19,37 @@ roll n = do
     gen <- newStdGen
     return $ rollDice n gen
 
+sort' :: Ord a => [a] -> [a]
+sort' [] = []
+sort' (a:[]) = [a]
+sort' l = minValue : sort' (except_first minValue l)
+    where minValue = minimum l
+
+except_first :: Eq a => a -> [a] -> [a]
+except_first _ [] = []
+except_first a (x:xs) = if (x==a) then xs else x:(except_first a xs)
+
+unzip' :: [(a,b)] -> ([a], [b])
+unzip' [] = ([],[])
+unzip' ((a,b):rest) = (a:as, b:bs)
+    where (as, bs) = unzip' rest
+--unzip' ((a,b):rest) = (a:(fst (unzip' rest)), b:(snd (unzip' rest)))
+
+factorial :: Int -> Int
+factorial 1 = 1
+factorial n = n * factorial (n-1)
+
+-- Comb n k: From n items, pick k
+comb :: [a] -> Int -> [[a]]
+comb _ 0 = [[]]
+comb (x:xs) k
+    | n < k = []
+    | n == k = [x:xs] 
+    | otherwise = combs_of_x ++ combs_of_xs
+    where
+        n = 1 + length xs
+        combs_of_x = map ([x]++) $ comb xs (k-1)
+        combs_of_xs = comb xs k
+
+
 
