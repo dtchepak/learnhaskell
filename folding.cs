f2e41694aa418c5aea7dd1c02e1bde2ae61fa0a3
David Tchepak <dave@davesquared.net>
2011-12-22 22:32:39 +1100

    Trying cons (used + operator on CList class)

--- folding.cs
+++ folding.cs
@@ -4,20 +4,47 @@ using System.Linq;
 
 public static class Folding {
 
-    public class MyList<A> {
-        private readonly int headIndex = 0;
-        private readonly A[] items;
+    public interface IHeadTailList<A> {
+        bool IsEmpty();
+        A Head();
+        IHeadTailList<A> Tail();
+    }
+
+    public class MyList<A> : IHeadTailList<A> {
+        readonly int headIndex = 0;
+        readonly A[] items;
         public MyList(params A[] items) { this.items = items; }
-        private MyList(int headIndex, params A[] items) { 
+        MyList(int headIndex, params A[] items) { 
             this.headIndex = headIndex;
             this.items = items; 
         }
         public bool IsEmpty() { return headIndex >= items.Length; }
         public A Head() { return items[headIndex]; }
-        public MyList<A> Tail() { return new MyList<A>(headIndex+1, items); }
+        public IHeadTailList<A> Tail() { return new MyList<A>(headIndex+1, items); }
     }
 
-    public static A FoldLeft<A,B>(Func<A, B, A> f, A accum, MyList<B> list) {
+    public class CList<A> : IHeadTailList<A> {
+        readonly bool empty;
+        readonly A head;
+        readonly CList<A> tail;
+        CList(A head, CList<A> tail) { this.head = head; this.tail = tail; this.empty = false; }
+        CList() { this.empty = true; }
+
+        public A Head() { return head; }
+        public IHeadTailList<A> Tail() { return tail; }
+        public bool IsEmpty() { return empty; }
+        public static readonly CList<A> Empty = new CList<A>();
+
+        public static CList<A> operator +(A head, CList<A> tail) {
+            return FoldRight((a,b) => new CList<A>(a, b), new CList<A>(head,Empty), tail);
+        }
+    }
+
+    public static MyList<A> Cons<A>(this A head, MyList<A> tail) {
+        return null;
+    }
+
+    public static A FoldLeft<A,B>(Func<A, B, A> f, A accum, IHeadTailList<B> list) {
         if (list.IsEmpty()) return accum;
         return FoldLeft(f, f(accum, list.Head()), list.Tail());
     }
@@ -40,7 +67,7 @@ public static class Folding {
         return f(list.First(), FoldRight(f, seed, list.Skip(1)));
     }
 
-    public static A FoldRight<A,B>(Func<B,A,A> f, A seed, MyList<B> list) {
+    public static A FoldRight<A,B>(Func<B,A,A> f, A seed, IHeadTailList<B> list) {
         if (list.IsEmpty()) return seed;
         return f(list.Head(), FoldRight(f, seed, list.Tail()));
     }
@@ -72,10 +99,15 @@ public static class Folding {
         //
         // mono folding.exe  6.26s user 0.01s system 99% cpu 6.279 total
 
-        var mylist = new MyList<int>(list);
-        var result = FoldRight((a,b) => a+b, 0, mylist);
+        //var mylist = new MyList<int>(list);
+        //var result = FoldRight((a,b) => a+b, 0, mylist);
         //
         // mono folding.exe  0.04s user 0.00s system 94% cpu 0.042 total
+        
+        // =====================
+
+        var clist = 1 + (2 + (3 + CList<int>.Empty));
+        var result = FoldLeft((a,b) => a+b, 0, clist);
 
         Console.WriteLine(result);
     }
