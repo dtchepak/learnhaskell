66a4d556443caf59096456358c26b6eceafc9f0e
David Tchepak <dave@davesquared.net>
2013-04-21 01:18:58 +1000

    attempt at adapter

--- producerconsumer.hs
+++ producerconsumer.hs
@@ -40,26 +40,34 @@ eval (Next a p) (Consume c) = eval p (c (Just a))
 
 sumUntilOdd :: Consumer Integer Integer
 sumUntilOdd = 
-    let loop i = Consume $ \x -> case x of
-                                Nothing -> Done i
-                                Just x' -> if even x' then loop (x' + i) else Done i
+    let loop i = Consume $ maybe (Done i) (\x -> if even x then loop (x+i) else Done i)
     in loop 0
 
 takeWhileP :: (i -> Bool) -> Producer i -> Producer i
 takeWhileP _ EndStream = EndStream
 takeWhileP pred (Next a p) = if pred a then Next a (takeWhileP pred p) else EndStream
 
+takeWhileC :: (i -> Bool) -> i -> Consumer i i
+takeWhileC p i = Consume $ maybe (Done i) (\x -> if p x then takeWhileC p i else Done i)
+
 sumC :: Consumer Integer Integer
 sumC =
     let loop i = Consume $ maybe (Done i) (loop . (i+)) 
     in loop 0
 
+adapt :: Consumer i o -> Consumer o o' -> Consumer i o'
+adapt _ (Done o') = Done o'
+adapt (Done o) (Consume c) = adapt (Done o) (Consume (const (c Nothing)))
+adapt (Consume c) c' = Consume $ \mi -> adapt (c mi) c' -- <--- rong
+
 --sumUntilOdd' :: Consumer Integer Integer
 --sumUntilOdd' = 
+--blarg :: Producer Integer -> Consumer Integer Integer
+--blarg EndStream =
 
---transform :: Consumer i o -> Producer o
---transform (Done o) = Next o EndStream
---transform (Consume c) =
+--transform :: Producer i -> Consumer i o -> Producer o
+--transform _ (Done o) = Next o EndStream
+--transform EndStream (Consume c) = EndStream
 --
 --
 tp x = trace (show x) x
