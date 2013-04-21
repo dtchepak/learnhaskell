639e7d78d87db0bff1b71375b6756560ea4753a5
David Tchepak <dave@davesquared.net>
2013-04-21 10:28:50 +1000

    functor, applicative and monad instances

--- producerconsumer.hs
+++ producerconsumer.hs
@@ -1,3 +1,5 @@
+import Control.Applicative
+import Control.Monad
 import Data.Maybe 
 import Debug.Trace
 
@@ -60,6 +62,44 @@ adapt _ (Done o') = Done o'
 adapt (Done o) (Consume c) = adapt (Done o) (Consume (const (c Nothing)))
 adapt (Consume c) c' = Consume $ \mi -> adapt (c mi) c' -- <--- rong
 
+getLineC :: Consumer Char String
+getLineC =
+    let loop s = Consume $ \mc -> case mc of
+                                    Just '\n' -> Done (reverse s)
+                                    Nothing   -> Done (reverse s)
+                                    Just c    -> loop (c:s)
+    in loop ""
+
+instance Functor (Consumer i) where
+    fmap f (Done o) = Done (f o)
+    fmap f (Consume c) = Consume $ \ma -> fmap f (c ma)
+
+joinConsumer :: Consumer i (Consumer i o) -> Consumer i o
+joinConsumer (Done c) = c
+joinConsumer (Consume c) = Consume $ \mi -> join (c mi)
+
+instance Applicative (Consumer i) where
+    f <*> a = f >>= \f' -> fmap f' a
+    pure = return
+
+instance Monad (Consumer i) where
+    return = Done
+    c >>= f = 
+        let joinC :: Consumer i (Consumer i o) -> Consumer i o
+            joinC (Done c) = c
+            joinC (Consume c) = Consume $ joinC . c
+        in joinC (fmap f c)
+    --(Done o) >>= f = f o
+    --(Consume c) >>= f = undefined --Consume (c >>> f)
+
+getLinesC :: Consumer Char [String]
+getLinesC = 
+    let loop s = getLineC >>= \l -> case l of
+                                        "" -> Done (reverse s)
+                                        _  -> loop (l:s)
+    in loop []
+
+
 --sumUntilOdd' :: Consumer Integer Integer
 --sumUntilOdd' = 
 --blarg :: Producer Integer -> Consumer Integer Integer
@@ -68,10 +108,8 @@ adapt (Consume c) c' = Consume $ \mi -> adapt (c mi) c' -- <--- rong
 --transform :: Producer i -> Consumer i o -> Producer o
 --transform _ (Done o) = Next o EndStream
 --transform EndStream (Consume c) = EndStream
---
---
+
 tp x = trace (show x) x
 sampleNum = produce [tp 2, tp 4, tp 6, tp 7, tp 8, tp 9]
 
 -- eval (takeWhileP even sampleNum) sumC
---
