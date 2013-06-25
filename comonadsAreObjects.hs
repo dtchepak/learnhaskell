2e23c2fd4670732af08f4d98330fcd6f6bafe58a
David Tchepak <dave@davesquared.net>
2013-06-25 22:51:43 +1000

    working on http://www.haskellforall.com/2013/02/you-could-have-invented-comonads.html

---
+++ comonadsAreObjects.hs
@@ -0,0 +1,14 @@
+type Option = String
+data Config = MakeConfig [Option] deriving (Show)
+
+configBuilder :: [Option] -> Config
+configBuilder = MakeConfig
+
+defaultConfig :: [Option] -> Config
+defaultConfig options = MakeConfig (["-Wall"] ++ options)
+
+profile :: ([Option] -> Config) -> Config
+profile builder = builder ["-prof", "-auto-all"]
+
+goFaster :: ([Option] -> Config) -> Config
+goFaster builder = builder ["-O2"]
