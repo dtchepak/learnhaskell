b13a188f747b3f3f04929a13cbac85d6a1a985b8
David Tchepak <dave@davesquared.net>
2014-06-17 14:35:24 +1000

    monoid sample

--- monoids.hs
+++ monoids.hs
@@ -1,16 +1,16 @@
+import Control.Applicative
 
-newtype Pair b a = Pair { getPair :: (a,b) }
+class Monoid a where
+    (<>) :: a -> a -> a
+    mzero :: a
 
-replace :: Pair a a -> a -> Pair a a
-replace (Pair { getPair = (x,y) }) z = Pair ((z,z))
+instance Monoid b => Monoid (a -> b) where
+    (<>) = liftA2 (<>)
+    mzero = const mzero
 
-instance Functor ((,) r) where
-    fmap f (a,b) = (a,f b)
+newtype And = And Bool
+    deriving (Show, Eq)
 
-data CoolBool = CoolBool { getCoolBool :: Bool }
-helloMe :: CoolBool -> String
-helloMe (CoolBool _) = "hello"
-
-newtype CoolBool2 = CoolBool2 { getCoolBool2 :: Bool }
-helloMe2 :: CoolBool2 -> String
-helloMe2 (CoolBool2 _) = "hello"
+instance Monoid And where
+    And a <> And b = And $ a && b
+    mzero = And True
