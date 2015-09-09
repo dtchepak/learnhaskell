073cd46c74d7f6d77d20976e613bbc910ad573da
David Tchepak <dave@davesquared.net>
2015-09-09 10:01:30 +1000

    rename fn

--- errors.hs
+++ errors.hs
@@ -4,8 +4,8 @@ import Control.Monad
 (?|) :: Maybe a -> e -> Either e a
 a ?| e = maybe (Left e) Right a
 
-sample :: (Int, Int) -> Either String (Int,Int)
-sample (a,b) = do
+someFn :: (Int, Int) -> Either String (Int,Int)
+someFn (a,b) = do
     guard (even a) ?| "first arg must be even"
     guard (b > 10) ?| "second arg must be >10"
     return (a,b)
